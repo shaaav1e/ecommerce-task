94a595a7f4dd6b81c05c186ea5bf0d756f752844
shaaav1e <shaaavie@gmail.com>
2025-05-15 14:59:43 +0500

    Quote section completed for mobile

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -3,24 +3,26 @@ import React from "react";
 const Quote = () => {
   return (
     <div className="mt-4">
-      <div className="bg-white container rounded-md">
-        <div className="flex justify-between bg rounded-md overflow-hidden">
-          <div className="w-1/2 flex flex-col gap-3 px-12 py-10 text-white">
-            <h1 className="font-semibold text-3xl tracking-wide">
+      <div className="bg-white md:container md:rounded-md">
+        <div className="flex justify-between bg md:rounded-md overflow-hidden">
+          <div className="md:w-1/2 flex flex-col gap-3 px-8 md:px-12 py-3 md:py-10 text-white">
+            <h1 className="font-semibold text-xl md:text-3xl tracking-wide">
               An easy way to send <br /> requests to all suppliers
             </h1>
-            <p className="font-light tracking-wider">
+            <p className="hidden md:font-light md:tracking-wider">
               Easily connect with all your suppliers in just a few clicks. Save
               time <br /> by sending requests in bulk.
             </p>
+            <button className="md:hidden bg-blue text-white font-medium rounded-md px-4 py-1 w-fit mt-3">
+              Send inquiry
+            </button>
           </div>
           {/* Form */}
-          <div className="bg-white text-black rounded-lg shadow-md p-8 mx-6 my-6 w-1/3">
+          <div className="hidden md:bg-white md:text-black md:rounded-lg md: shadow-md md:p-8 md:mx-6 md:my-6 md:w-1/3">
             <h3 className="text-lg font-semibold mb-4">
               Send quote to suppliers
             </h3>
             <form className="flex flex-col gap-4">
-              {" "}
               <input
                 type="text"
                 placeholder="What item you need?"
@@ -31,7 +33,6 @@ const Quote = () => {
                 className="w-full border border-gray-200 rounded-md px-2 py-3 text-md h-24 text-black focus:outline-none font-light placeholder-gray-400"
               ></textarea>
               <div className="flex gap-2">
-                {" "}
                 <input
                   type="text"
                   placeholder="Quantity"
